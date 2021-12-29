7d770d06333429cbae97fdcb14792f536623a3c4
Aref111n <71724445+Aref111n@users.noreply.github.com>
2021-12-30 00:49:56 +0600

    Update Even sum or Odd sum.cpp

--- Basic/Even sum or Odd sum.cpp
+++ Basic/Even sum or Odd sum.cpp
@@ -1,3 +1,23 @@
+/*
+Problem A: Sum of Even/Odd
+Description:
+Write an object oriented program that reads a set of non-negative integer numbers and output the sum of odd numbers and even numbers from that. To create
+the program, you have to write a class name IntegerFactory where there will be
+an integer array as private variable. There will also be public member functions
+that will responsible for providing the sums.
+Input Format:
+Program will read an integer n, followed by n integers and an integer c for choice
+to show either the sum of even number or the sum of odd numbers of these n
+integers. c=0 for sum of odd numbers and c=1 for sum of even numbers.
+Constraints:
+0 < n <= 10000, 0 <= c <= 1
+Sample Input:
+5
+1 2 3 4 5
+0
+Sample Output:
+6
+*/
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
@@ -38,7 +58,7 @@ int main()
     a.get(n) ;
     ll c, ans ;
     cin >> c ;
-    if(c==0)
+    if(c==1)
         ans = a.even_sum(n) ;
     else
         ans = a.odd_sum(n) ;
